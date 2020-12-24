105b70d70190b20a1d47dae919c0b109b629eaee
nuraiusa <nurayberdalieva@gmail.com>
2020-12-24 02:02:07 +0000

    added some files

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -1,11 +1,13 @@
-from flask import Flask,  jsonify 
-import os 
+from flask import Flask, jsonify
+import os
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'hello world',
+        'message': 'Hello World',
         'environment': os.environ.get('ENVIRONMENT')
     })
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
